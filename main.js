cafe80c80a340a35befcdff2bc8020dfdbb88e94
Balamurali Sundaramoorthy <balamurali.sundar@outlook.com>
2016-08-02 23:16:48 +0000

    first test complete

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 "use strict";
 /// <reference path="typings/index.d.ts" />
-const java = require('java');
-const path = require('path');
-//java.classpath.push(path.join(__dirname, 'java', 'HH_PPS_V_API.jar'));
+var java = require('java');
+var path = require('path');
+var fs = require('fs');
+java.classpath.push(path.join(__dirname, 'grouper', 'java', 'HH_PPS_V_API.jar'));
 java.classpath.push(path.join(__dirname, 'grouper', 'java', 'HomeHealthJava.jar'));
-let HH_PPS = java.import('com.mmm.cms.homehealth.pps.HH_PPS');
-let hhps = HH_PPS.getInstanceSync();
-let File = java.import('java.io.File');
-let configFile = new File(path.join(__dirname, ''));
-let javaFile = java.newInstanceSync('java.io.File', path.join(__dirname, ''));
-hhps.init();
-//let javaFile = java.newInstanceSync('java.io.File', path.join(__dirname, ''))
-hhps.init();
-//# sourceMappingURL=main.js.map
+var HH_PPS = java.import('com.mmm.cms.homehealth.pps.HH_PPS');
+var hhps = HH_PPS.getInstanceSync();
+var File = java.import('java.io.File');
+var configFile = new File(path.join(__dirname, 'grouper', 'HomeHealthGrouper.properties'));
+hhps.init(configFile, function () {
+    var testData = fs.readFileSync(path.join(__dirname, 'grouper', 'test-files', 'v5216', 'XMLFile_1.XML'), 'utf8');
+    var scoreResult = hhps.scoreRecord(testData, function (err, result) {
+        if (!err) {
+            var exception = result.getExceptionSync();
+            if (exception) {
+                console.log(exception.getMessageSync());
+            }
+            else {
+                console.log(result.getHIPPSCodeSync().getCodeSync());
+            }
+        }
+    });
+});
--- main.ts
+++ main.ts
@@ -1,16 +1,30 @@
 /// <reference path="typings/index.d.ts" />
 import * as java from 'java';
 import * as path from 'path';
+import * as fs from 'fs';
 
-//java.classpath.push(path.join(__dirname, 'java', 'HH_PPS_V_API.jar'));
+java.classpath.push(path.join(__dirname, 'grouper', 'java', 'HH_PPS_V_API.jar'));
 java.classpath.push(path.join(__dirname, 'grouper', 'java', 'HomeHealthJava.jar'));
 let HH_PPS = java.import('com.mmm.cms.homehealth.pps.HH_PPS');
 let hhps = HH_PPS.getInstanceSync();
 
 let File = java.import('java.io.File');
-let configFile = new File(path.join(__dirname, ''));
+let configFile = new File(path.join(__dirname, 'grouper', 'HomeHealthGrouper.properties'));
 
-let javaFile = java.newInstanceSync('java.io.File', path.join(__dirname, ''))
-hhps.init();
-//let javaFile = java.newInstanceSync('java.io.File', path.join(__dirname, ''))
-hhps.init();
+
+hhps.init(configFile, ()=>{
+    let testData = fs.readFileSync(path.join(__dirname, 'grouper', 'test-files', 'v5216', 'XMLFile_1.XML'), 'utf8');
+    let scoreResult = hhps.scoreRecord(testData, (err, result)=>{
+        if (!err){
+            let exception = result.getExceptionSync();
+            if (exception){
+                console.log(exception.getMessageSync());    
+            } else {
+                console.log(result.getHIPPSCodeSync().getCodeSync());
+            }
+            
+        }
+
+    });    
+});
+
