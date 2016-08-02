f2c434ca565add7dcae8277e7b97c1912bf6bc75
Sundaramoorthy <SundaramoorthyB@mkcorp.com>
2016-08-02 17:04:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+//npm node-gyp certificate error
+//https://github.com/nodejs/node-gyp/issues/695
+set NODE_TLS_REJECT_UNAUTHORIZED=0
---
+++ main.ts
@@ -0,0 +1,16 @@
+/// <reference path="typings/index.d.ts" />
+import * as java from 'java';
+import * as path from 'path';
+
+//java.classpath.push(path.join(__dirname, 'java', 'HH_PPS_V_API.jar'));
+java.classpath.push(path.join(__dirname, 'grouper', 'java', 'HomeHealthJava.jar'));
+let HH_PPS = java.import('com.mmm.cms.homehealth.pps.HH_PPS');
+let hhps = HH_PPS.getInstanceSync();
+
+let File = java.import('java.io.File');
+let configFile = new File(path.join(__dirname, ''));
+
+let javaFile = java.newInstanceSync('java.io.File', path.join(__dirname, ''))
+hhps.init();
+//let javaFile = java.newInstanceSync('java.io.File', path.join(__dirname, ''))
+hhps.init();
